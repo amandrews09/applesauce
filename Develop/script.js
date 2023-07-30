e4250805a3d9f096cb696bc0c061173bf36a2d3c
Amandrews <amandrews09@gmail.com>
2023-07-29 20:29:20 -0600

    added arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,23 @@
 // Assignment code here
 
+// Array of special characters
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+// Array of numbers
+var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// Array of lowercase letters
+var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h,', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+// Array of uppercase letters
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 function myFunction() {
-  let passwordLength = prompt("choose a password length of at least 8 characters and no more than 128 characters");
+  let passwordLength = prompt("Pick a password length between 8 and 128");
   if (passwordLength < 8) {
     alert('Password length must be at least 8 characters');
     return;  
   }
   if (passwordLength > 128) {
-  alert('Password length must be less than 128 characters');
-  return;
+    alert('Password length must be less than 129 characters');
+    return;
   }
 }
 
